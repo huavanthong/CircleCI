19c94d17845a1626d2db3c854df71d9718b5de75
htv3hc <thong.huavan@vn.bosch.com>
2023-09-07 15:35:05 +0700

    Add test case for special characters.

--- hello_test.go
+++ hello_test.go
@@ -31,3 +31,14 @@ func TestMessageLackOfName(t *testing.T) {
 		t.Errorf("Incorrect message: %s", msg)
 	}
 }
+
+func TestMessageWithSpecialCharacters(t *testing.T) {
+	project := new(Project)
+	project.PushedAt = "now@@@#!!'''"
+	project.Name = "TestProject!@#$%^&*()'@''"
+
+	msg := Message(project)
+	if msg != "<p>TestProject!@#$%^&*()'@'': Latest commit: now@@@#!!'''</p>" {
+		t.Errorf("Incorrect message: %s", msg)
+	}
+}
