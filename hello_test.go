e0d3305273524eaa9d490d07070607012fbadbe5
htv3hc <thong.huavan@vn.bosch.com>
2023-10-09 17:42:47 +0700

    Restrecutre yaml file for avoid many linea

--- hello_test.go
+++ hello_test.go
@@ -42,3 +42,27 @@ func TestFailCase(t *testing.T) {
 		t.Fail() // Bắt lỗi ở đây
 	}
 }
+
+func TestFailCaseSystemError(t *testing.T) {
+	// Arrange
+	project := new(Project)
+	project.PushedAt = "sssss"
+	panic(1)
+	// Act
+	func() {
+		defer func() {
+			if r := recover(); r != nil {
+				// Assert
+				t.Logf("Expected system error: %v", r)
+				return
+			}
+		}()
+
+		// This will cause a panic and a system error
+		msg := Message(project)
+		_ = msg // To avoid "unused variable" warning
+	}()
+
+	// If the code reaches here, the test should fail
+	t.Fail()
+}
